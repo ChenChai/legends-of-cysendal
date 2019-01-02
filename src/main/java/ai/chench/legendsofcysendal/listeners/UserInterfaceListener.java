659d9acd086f5a3e98c5e8f81f327af0e3a075d5
ChenChai <chen.chai@outlook.com>
2019-01-02 13:46:22 -0700

    Added listener to open screen on new player join

---
+++ src/main/java/ai/chench/legendsofcysendal/listeners/UserInterfaceListener.java
@@ -0,0 +1,87 @@
+package ai.chench.legendsofcysendal.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInterfaceListener implements Listener {
+
+    private Plugin plugin;
+    public UserInterfaceListener(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        plugin.reloadConfig(); // make sure config up to date
+        Player player = event.getPlayer();
+
+        String uniqueId = player.getUniqueId().toString();
+
+        // check if player is registered in config.yml
+        // getConfig().isBoolean will return false if the path does not exist because it is the player's first time joining.
+        if (!plugin.getConfig().isBoolean("players." + uniqueId + ".firstJoin") || plugin.getConfig().getBoolean("players." + uniqueId + ".firstJoin")) {
+            resetPlayer(player);
+        }
+    }
+
+    // returns player to 0 sp and classless and
+    // brings up interface to choose a new class
+    private void resetPlayer(Player player) {
+        // initialize player in config
+        String uniqueId = player.getUniqueId().toString();
+        plugin.getConfig().set("players." + uniqueId + ".sp", 0); // set number of soul points to 0
+        plugin.getConfig().set("players." + uniqueId + ".level", 1); // set level to 1
+        plugin.getConfig().set("players." + uniqueId + ".class", "none");
+        plugin.getConfig().set("players." + uniqueId + ".firstJoin", true); // this is effectively the player's first time joining
+
+        plugin.saveConfig();
+
+        final Inventory selectClass = Bukkit.createInventory(null, 54, ChatColor.BLACK + "" + ChatColor.BOLD + "Welcome to Legends of Cysendal!");
+
+        // players will hover over this book to read an introduction to LoC.
+        ItemStack introBook = new ItemStack(Material.BOOK, 1);
+        //introBook.addUnsafeEnchantment(Enchantment.LOYALTY, 0);
+        ItemMeta itemMeta = introBook.getItemMeta();
+
+        List<String> lore = new ArrayList<String>();
+        lore.add("Welcome, hero.");
+
+        //lore.add("Click to continue.");
+
+        itemMeta.setLore(lore);
+        introBook.setItemMeta(itemMeta);
+        selectClass.setItem(1, introBook);
+
+
+        class myRunnable implements Runnable {
+            Player player;
+            Inventory inventory;
+            public myRunnable(Player player, Inventory inventory) {
+                this.player = player;
+                this.inventory = inventory;
+            }
+
+
+            public void run() {
+                player.openInventory(inventory);
+            }
+        }
+
+        Bukkit.getScheduler().runTaskLater(plugin, new myRunnable(player, selectClass), 5);
+
+    }
+
+}
