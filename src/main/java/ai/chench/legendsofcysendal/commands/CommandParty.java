2fd4b2dc1c377e4401374915e429ebf72213a402
ChenChai <chen.chai@outlook.com>
2019-01-13 16:07:57 -0500

    Added party disbanding and party help message

--- src/main/java/ai/chench/legendsofcysendal/commands/CommandParty.java
+++ src/main/java/ai/chench/legendsofcysendal/commands/CommandParty.java
@@ -2,6 +2,7 @@ package ai.chench.legendsofcysendal.commands;
 
 import ai.chench.legendsofcysendal.Main;
 import ai.chench.legendsofcysendal.util.PartyManager;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,30 +17,39 @@ public class CommandParty implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            return false;
+            sender.sendMessage("You are not a player. Only players can manage parties.");
         }
 
         Player player = (Player) sender;
+        PartyManager partyManager = new PartyManager(main);
 
         // /party create <partyName>
         if (args.length >= 2 && args[0].equalsIgnoreCase("create")) {
-            PartyManager partyManager = new PartyManager(main);
             partyManager.createParty(player, args[1]);
-
-            player.sendMessage(main.getConfig().getString("messages.party.partyCreated"));
             return true;
         }
 
 
+        // /party disband
         if (args.length >= 1 && args[0].equalsIgnoreCase("disband")) {
-
+            partyManager.disbandParty(player);
+            return true;
         }
 
 
-
-        return false;
+        sendPartyInstructions(player);
+        return true;
     }
 
-
+    // gives the player a party help menu.
+    public void sendPartyInstructions(Player player) {
+        player.sendMessage(ChatColor.DARK_GRAY + "=====================");
+        player.sendMessage(ChatColor.BOLD + "PARTY HELP");
+        player.sendMessage(ChatColor.DARK_GRAY + "=====================");
+        player.sendMessage(ChatColor.BLUE + "/party help:" + ChatColor.WHITE + " Brings up help menu.");
+        player.sendMessage(ChatColor.BLUE + "/party create <name>:" + ChatColor.WHITE + " Creates a party with the specified name. Only one party can have a specific name at a time.");
+        player.sendMessage(ChatColor.BLUE + "/party disband:" + ChatColor.WHITE + " Disbands a party. Only party leaders can use this command.");
+        player.sendMessage(ChatColor.DARK_GRAY + "=====================");
+    }
 
 }
--- src/main/java/ai/chench/legendsofcysendal/util/PartyManager.java
+++ src/main/java/ai/chench/legendsofcysendal/util/PartyManager.java
@@ -7,6 +7,23 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
+// this class handles the creation and management of parties, which are lists of players with a leader, stored in configuration.
+// a player's party is stored as a party name in their config. Party names are unique, and a party is identified by its party name.
+// party data is stored in the playerData.yml as well, under the heading 'parties.<partyName>' each party entry has a list of players,
+// the party's leader, and a party's pending invites.
+
+// How the party looks in YAML
+// parties:
+//   party_name_here:
+//     active: true
+//     leader: uuid_string_1
+//     members:
+//     - uuid_string_1
+//     - uuid_string_2
+//     invites:
+//     - uuid_string_3
+
+
 public class PartyManager {
 
     Main main;
@@ -14,17 +31,17 @@ public class PartyManager {
         this.main = main;
     }
 
-    public boolean createParty(Player partyLeader, String partyName) {
+    public boolean createParty(Player partyCreator, String partyName) {
 
         // if the person creating the party is already in a party
-        if (main.getPlayerDataConfig().getString("players." + partyLeader.getUniqueId().toString() + ".party", null) != null) {
-            partyLeader.sendMessage(ChatColor.YELLOW + main.getConfig().getString("errors.party.alreadyInParty"));
+        if (main.getPlayerDataConfig().getString("players." + partyCreator.getUniqueId().toString() + ".party", null) != null) {
+            partyCreator.sendMessage(ChatColor.YELLOW + main.getConfig().getString("errors.party.alreadyInParty"));
             return false;
         }
 
         // if a party under the same name is already active
         if (main.getPlayerDataConfig().getBoolean("parties." + partyName + ".active", false)) {
-            partyLeader.sendMessage(ChatColor.YELLOW + main.getConfig().getString("errors.party.partyNameTaken"));
+            partyCreator.sendMessage(ChatColor.YELLOW + main.getConfig().getString("errors.party.partyNameTaken"));
             return false;
         }
 
@@ -33,18 +50,50 @@ public class PartyManager {
 
         // add the party creator to the party's member list
         List<String> playerList = new ArrayList<String>();
-        playerList.add(partyLeader.getUniqueId().toString());
+        playerList.add(partyCreator.getUniqueId().toString());
         main.getPlayerDataConfig().set("parties." + partyName + ".members", playerList);
 
         // add the party creator as the party leader.
-        main.getPlayerDataConfig().set("parties." + partyName + ".leader", partyLeader.getUniqueId().toString());
+        main.getPlayerDataConfig().set("parties." + partyName + ".leader", partyCreator.getUniqueId().toString());
 
         // set the party creator's current party to this one.
-        main.getPlayerDataConfig().set("players." + partyLeader.getUniqueId().toString() + ".party", partyName);
+        main.getPlayerDataConfig().set("players." + partyCreator.getUniqueId().toString() + ".party", partyName);
 
         main.savePlayerDataConfig();
+
+        partyCreator.sendMessage(main.getConfig().getString("messages.party.partyCreated"));
         return true;
     }
 
+    // attempts to disband the player's party. Only works if they are the party leader.
+    public boolean disbandParty(Player disbandingPlayer) {
+        String playerUid = disbandingPlayer.getUniqueId().toString();
 
+        String partyName = main.getPlayerDataConfig().getString("players." + playerUid + ".party", null);
+
+        // check if the player is actually in a party.
+        if (partyName == null) {
+            disbandingPlayer.sendMessage(main.getConfig().getString("errors.party.notInParty"));
+            return false;
+        }
+
+        // check if the player is the party leader.
+        String leaderUid = main.getPlayerDataConfig().getString("parties." + partyName + ".leader");
+        if (!playerUid.equalsIgnoreCase(leaderUid)) {
+            disbandingPlayer.sendMessage(main.getConfig().getString("errors.party.notPartyLeader"));
+            return false;
+        }
+
+        main.getPlayerDataConfig().set("parties." + partyName + ".leader", null);
+        main.getPlayerDataConfig().set("parties." + partyName + ".invites", null);
+        main.getPlayerDataConfig().set("parties." + partyName + ".members", null);
+        main.getPlayerDataConfig().set("parties." + partyName + ".active", false);
+
+        main.getPlayerDataConfig().set("players." + playerUid + ".party", null);
+
+        main.savePlayerDataConfig();
+
+        disbandingPlayer.sendMessage(main.getConfig().getString("messages.party.partyDisbanded"));
+        return true;
+    }
 }
